61878f56b9e401812bcf28e09ff828896fa5edf2
Contreras399 <cc5913367@gmail.com>
2024-10-27 21:57:20 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace xdlol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.lblMensaje.Location = new System.Drawing.Point(299, 153);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(388, 39);
+            this.lblMensaje.TabIndex = 0;
+            this.lblMensaje.Text = "¿Quieres ser mi novia?";
+            // 
+            // btnSi
+            // 
+            this.btnSi.BackColor = System.Drawing.Color.Lime;
+            this.btnSi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnSi.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnSi.Location = new System.Drawing.Point(262, 266);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(166, 118);
+            this.btnSi.TabIndex = 1;
+            this.btnSi.Text = "Si";
+            this.btnSi.UseVisualStyleBackColor = false;
+            this.btnSi.Click += new System.EventHandler(this.btnSi_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnNo.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnNo.Location = new System.Drawing.Point(554, 266);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(166, 118);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = false;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1373, 626);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnSi);
+            this.Controls.Add(this.lblMensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Button btnNo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xdlol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSi_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("¡Sabía que diías que sí, te amo!");
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int maxX = this.Size.Width - btnNo.Width;
+            int maxY = this.Size.Height - btnNo.Height;
+
+            int newX = random.Next(0, maxX);
+            int newY = random.Next(0, maxY);
+
+            btnNo.Location = new Point(newX, newY);
+        }
+    }
+}
